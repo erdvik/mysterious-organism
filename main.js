04b5bf24a45a7c669288b2f95712fbb605d6e2ab
rdv1k <43339320+rdv1k@users.noreply.github.com>
2022-01-23 13:06:58 +0100

    added organism factory function

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -13,7 +13,15 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+  }
+}
 
+let organism = pAequorFactory(1, mockUpStrand());
+console.log(organism);
 
 
 
